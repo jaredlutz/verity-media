8a28c9291ac50297b370a8baf5013247e7841340
Rishabh Dixit <r.dixit99@hotmail.com>
2023-02-12 19:12:08 +0530

    refactor: removed unnecessary comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,125 +3,16 @@ import requests from "@/utils/requests";
 
 import { IMovieAPIResponse } from "@/types";
 
-type DataResponseType = {
-  trendingNow: IMovieAPIResponse;
-  popularMovies: IMovieAPIResponse;
-  discoverMovies: IMovieAPIResponse;
-  topRatedMovies: IMovieAPIResponse;
-  actionMovies: IMovieAPIResponse;
-};
+// type DataResponseType = {
+//   trendingNow: IMovieAPIResponse;
+//   popularMovies: IMovieAPIResponse;
+//   discoverMovies: IMovieAPIResponse;
+//   topRatedMovies: IMovieAPIResponse;
+//   actionMovies: IMovieAPIResponse;
+// };
 
 async function getData() {
   const res = await fetch(requests.fetchTrending);
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // const p1 = new Promise(async (resolve, reject) => {
-  //   const res = await fetch(requests.fetchTrending);
-
-  //   if (!res.ok) {
-  //     // This will activate the closest `error.js` Error Boundary
-  //     return reject(new Error("Failed to fetch data"));
-  //   }
-
-  //   return resolve(res.json());
-  // });
-  // const p2 = new Promise(async (resolve, reject) => {
-  //   const res = await fetch(requests.fetchPopularMovies);
-
-  //   if (!res.ok) {
-  //     // This will activate the closest `error.js` Error Boundary
-  //     return reject(new Error("Failed to fetch data"));
-  //   }
-
-  //   return resolve(res.json());
-  // });
-  // const p3 = new Promise(async (resolve, reject) => {
-  //   const res = await fetch(requests.fetchDiscoverMovies);
-
-  //   if (!res.ok) {
-  //     // This will activate the closest `error.js` Error Boundary
-  //     return reject(new Error("Failed to fetch data"));
-  //   }
-
-  //   return resolve(res.json());
-  // });
-  // const p4 = new Promise(async (resolve, reject) => {
-  //   const res = await fetch(requests.fetchTopRatedMovies);
-
-  //   if (!res.ok) {
-  //     // This will activate the closest `error.js` Error Boundary
-  //     return reject(new Error("Failed to fetch data"));
-  //   }
-
-  //   return resolve(res.json());
-  // });
-  // const p5 = new Promise(async (resolve, reject) => {
-  //   const res = await fetch(requests.fetchActionMovies);
-
-  //   if (!res.ok) {
-  //     // This will activate the closest `error.js` Error Boundary
-  //     return reject(new Error("Failed to fetch data"));
-  //   }
-
-  //   return resolve(res.json());
-  // });
-
-  // const [
-  //   trendingNow,
-  //   popularMovies,
-  //   discoverMovies,
-  //   topRatedMovies,
-  //   actionMovies,
-  // ] = await Promise.all([p1, p2, p3, p4, p5]);
-
-  // return {
-  //   trendingNow,
-  //   popularMovies,
-  //   discoverMovies,
-  //   topRatedMovies,
-  //   actionMovies
-  // };
-
-  // const [
-  //   trendingNow,
-  //   popularMovies,
-  //   discoverMovies,
-  //   topRatedMovies,
-  //   actionMovies,
-  //   comedyMovies,
-  //   horrorMovies,
-  //   romanceMovies,
-  //   documentaries,
-  // ] = await Promise.all([
-  //   fetch(requests.fetchTrending)
-  //     .then((res) => res.json())
-  //     .catch((err) => console.error(err)),
-  //   fetch(requests.fetchPopularMovies)
-  //     .then((res) => res.json())
-  //     .catch((err) => console.error(err)),
-  //   fetch(requests.fetchDiscoverMovies)
-  //     .then((res) => res.json())
-  //     .catch((err) => console.error(err)),
-  //   fetch(requests.fetchTopRatedMovies)
-  //     .then((res) => res.json())
-  //     .catch((err) => console.error(err)),
-  //   fetch(requests.fetchActionMovies)
-  //     .then((res) => res.json())
-  //     .catch((err) => console.error(err)),
-  //   fetch(requests.fetchComedyMovies)
-  //     .then((res) => res.json())
-  //     .catch((err) => console.error(err)),
-  //   fetch(requests.fetchHorrorMovies)
-  //     .then((res) => res.json())
-  //     .catch((err) => console.error(err)),
-  //   fetch(requests.fetchRomanceMovies)
-  //     .then((res) => res.json())
-  //     .catch((err) => console.error(err)),
-  //   fetch(requests.fetchDocumentaries)
-  //     .then((res) => res.json())
-  //     .catch((err) => console.error(err)),
-  // ]);
 
   // Recommendation: handle errors
   if (!res.ok) {
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,9 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-
-import { HeaderBellIcon, HeaderSearchIcon } from "@/svg";
 import useScrollY from "@/hooks/useScrollY";
 
-// interface ChildrenProps {
-//   children?: ReactNode;
-// }
-
-// interface IHeaderComposition {
-//   children?: JSX.Element;
-//   Container: FC<ChildrenProps>;
-//   LeftMenu: FC<ChildrenProps>;
-//   Logo: FC;
-//   Menu: FC;
-//   RightMenu: FC<ChildrenProps>;
-//   Search: FC;
-//   Notification: FC;
-// }
+import { HeaderBellIcon, HeaderSearchIcon } from "@/svg";
 
 const Header = () => {
   const isScrolled = useScrollY();
@@ -72,57 +57,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// Header.Container = function HeaderContainer({ children }: ChildrenProps) {
-//   return <div className="flex items-center justify-between">{children}</div>;
-// };
-
-// Header.LeftMenu = function HeaderLeftMenu({ children }: ChildrenProps) {
-//   return <div className="flex items-center gap-16">{children}</div>;
-// };
-
-// Header.Menu = function HeaderMenu() {
-//   return (
-//     <ul className="flex items-center gap-8">
-//       <li className="header-link">Home</li>
-//       <li className="header-link">TV Shows</li>
-//       <li className="header-link">Movies</li>
-//       <li className="header-link">New & Popular</li>
-//       <li className="header-link">My List</li>
-//     </ul>
-//   );
-// };
-
-// Header.Logo = function HeaderLogo() {
-//   return (
-//     <Link href="/">
-//       <Image
-//         src="./logo.svg"
-//         priority
-//         alt="Netflux logo"
-//         height={40}
-//         width={100}
-//       />
-//     </Link>
-//   );
-// };
-
-// Header.RightMenu = function HeaderRightMenu({ children }: ChildrenProps) {
-//   return <div className="flex items-center gap-6">{children}</div>;
-// };
-
-// Header.Search = function HeaderSearch() {
-//   return (
-//     <button>
-//       <HeaderSearchIcon />
-//     </button>
-//   );
-// };
-
-// Header.Notification = function HeaderNotification() {
-//   return (
-//     <button>
-//       <HeaderBellIcon />
-//     </button>
-//   );
-// };
