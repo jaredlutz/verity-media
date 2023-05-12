59944718361b140464943e805580cc2ff24e6a6c
Jared Lutz <jared.lutz@matchback.io>
2023-05-11 22:34:26 -0700

    vmedia

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -2,34 +2,34 @@ import Accordion from "../Accordion";
 
 const data = [
   {
-    item: "What is Netflux?",
+    item: "What is VMedia?",
     content:
-      "Netflux is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!",
+      "VMedia is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!",
   },
   {
-    item: "How much does Netflux cost?",
+    item: "How much does VMedia cost?",
     content:
-      "Watch Netflux on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹ 149 to ₹ 649 a month. No extra costs, no contracts.",
+      "Watch VMedia on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹ 149 to ₹ 649 a month. No extra costs, no contracts.",
   },
   {
     item: "Where can I watch?",
     content:
-      "Watch anywhere, anytime. Sign in with your Netflux account to watch instantly on the web at netflux.com from your personal computer or on any internet-connected device that offers the Netflux app, including smart TVs, smartphones, tablets, streaming media players and game consoles.You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflux with you anywhere.",
+      "Watch anywhere, anytime. Sign in with your VMedia account to watch instantly on the web at VMedia.com from your personal computer or on any internet-connected device that offers the VMedia app, including smart TVs, smartphones, tablets, streaming media players and game consoles.You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take VMedia with you anywhere.",
   },
   {
     item: "How do I cancel?",
     content:
-      "Netflux is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.",
+      "VMedia is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.",
   },
   {
-    item: "What can I watch on Netflux?",
+    item: "What can I watch on VMedia?",
     content:
-      "Netflux has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflux originals, and more. Watch as much as you want, anytime you want.",
+      "VMedia has an extensive library of feature films, documentaries, TV shows, anime, award-winning VMedia originals, and more. Watch as much as you want, anytime you want.",
   },
   {
-    item: "Is Netflux good for kids?",
+    item: "Is VMedia good for kids?",
     content:
-      "The Netflux Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
+      "The VMedia Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
   },
 ];
 
