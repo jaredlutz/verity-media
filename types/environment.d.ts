56ea021e0cb0210d2c77790cacfc57081a699355
Jared Lutz <jared.lutz@matchback.io>
2023-05-12 00:40:24 -0700

    environment

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -1,8 +1,8 @@
 namespace NodeJS {
   interface ProcessEnv extends NodeJS.ProcessEnv {
-    DATABASE_URL: string;
+    DATABASE_URL: postgres://default:XHF6UcJWi3qN@ep-falling-leaf-598252-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb;
     SHADOW_DATABASE_URL: string;
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: string;
-    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: string;
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: 814910024141-cft2msdbbsb4l0comrl3tde0akgf6ji7.apps.googleusercontent.com;
+    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: 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;
   }
 }
